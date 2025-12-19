227bb9b1794f2566561b680ed538ffec82dba4fb
zuice <me@jeffa.dev>
2025-12-19 16:22:39 -0500

    Initial

---
+++ fetch.lua
@@ -0,0 +1,17 @@
+local function fetch(repo, plugin_path)
+	local name = repo:gsub(".*/", "")
+	print("🚚 [got] fetching " .. name .. "...")
+
+	local tmp_zip = "/tmp/" .. name .. ".zip"
+	local tmp_dir = "/tmp/" .. name .. "_extract"
+
+	vim.fn.system({ "curl", "-L", "https://github.com/" .. repo .. "/archive/refs/heads/main.zip", "-o", tmp_zip })
+	vim.fn.system({ "unzip", "-q", tmp_zip, "-d", tmp_dir })
+
+	local internal = vim.fn.glob(tmp_dir .. "/*")
+	vim.fn.system({ "mkdir", "-p", plugin_path })
+	vim.fn.system({ "mv", internal, plugin_path })
+	vim.fn.system({ "rm", "-rf", tmp_zip, tmp_dir })
+end
+
+return fetch
